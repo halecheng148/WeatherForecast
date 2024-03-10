6dc2d7832d9a48a365bb33caf29762b3c3ec436f
halecheng <halecheng148@gmail.com>
2024-03-11 07:55:49 +0800

    "完成页面切换"

--- qweatherforecast.cpp
+++ qweatherforecast.cpp
@@ -12,3 +12,23 @@ QWeatherForecast::~QWeatherForecast()
 {
     delete ui;
 }
+
+void QWeatherForecast::on_switchModeBtn_clicked()
+{
+    QString strText = ui->switchModeBtn->text();
+
+    if(strText == "实时天气")
+        strText = "预报天气";
+    else
+        strText = "实时天气";
+
+    int index = ui->stackedWidget->currentIndex();
+    QWidget* page = ui->stackedWidget->currentWidget();
+    if(ui->page == page)
+        page = ui->page_2;
+    else
+        page = ui->page;
+
+    ui->switchModeBtn->setText(strText);
+    ui->stackedWidget->setCurrentWidget(page);
+}
--- qweatherforecast.h
+++ qweatherforecast.h
@@ -15,6 +15,13 @@ public:
     explicit QWeatherForecast(QWidget *parent = 0);
     ~QWeatherForecast();
 
+private slots:
+    /**
+     * @brief on_switchModeBtn_clicked
+     * 切换页面，有两个页面：（page="实时天气"、page_2="预报天气"
+     */
+    void on_switchModeBtn_clicked();
+
 private:
     Ui::QWeatherForecast *ui;
 };
